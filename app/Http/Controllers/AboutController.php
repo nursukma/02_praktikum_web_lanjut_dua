284344957f1698a0ead52c24a3c3de38827c9be3
nursukma <nursukma2k17@gmail.com>
2021-03-02 13:25:10 +0700

    Pertemuan 2 Praktikum 1

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "NIM : 1941720032 <br> Nama : Nur Sukma P";
+    }
+}
